1634ff792eada32e2b00b6242a465b1d8a478d0a
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 01:36:37 +0200

    Add: hobbies route.

--- forum/src/App.jsx
+++ forum/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Details from "./Details";
+import Hobbies from "./Hobbies";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/details" element={<Details />} />
+                <Route path="/hobbies" element={<Hobbies />} />
             </Routes>
         </BrowserRouter>
     );
