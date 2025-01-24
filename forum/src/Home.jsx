945c3621ef1661bc2b4a5f5b330558dc3ed767ad
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 01:01:41 +0200

    Change: default functionality for displayingposts in home.json.

--- forum/src/Home.jsx
+++ forum/src/Home.jsx
@@ -1,7 +1,20 @@
+import home from "./json/home.json";
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <div>
-            <h1>Hello!</h1>
+            <h1>Home</h1>
+            <ul>
+                {home.posts.map((post) => (
+                    <li key={post.id}>
+                        <Link to="/">{post.title}</Link>
+                        <p>
+                            julkaisija: {post.name} pvm: {post.date}
+                        </p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
