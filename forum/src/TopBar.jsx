3338db2bc676d48702975118e9ee3da6425ab6d5
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 15:57:33 +0200

    Add: old timy styling.

--- forum/src/TopBar.jsx
+++ forum/src/TopBar.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
+import "./TopBar.css";
+import home from "../public/home.png";
+import planet from "../public/planet.png";
 
 export default function TopBar() {
     return (
-        <div>
-            <h1>This is the topbar</h1>
-            <Link to={"/"}>Koti</Link>
-            <Link to={"/hobbies"}>Harrastukset</Link>
+        <div className="topBar">
+            <h1>Ulkoilmaharrastajat</h1>
+            <div className="nav">
+                <img src={home} alt="Home icon" />
+                <Link to={"/"}>Koti</Link>
+                <img src={planet} alt="planet icon" />
+                <Link to={"/hobbies"}>Harrastukset</Link>
+            </div>
         </div>
     );
 }
