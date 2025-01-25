4422d281244f377a31ef606d2761099ca560cc3e
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 21:11:22 +0200

    Add: login system.

--- forum/src/TopBar.jsx
+++ forum/src/TopBar.jsx
@@ -2,17 +2,91 @@ import { Link } from "react-router-dom";
 import "./TopBar.css";
 import home from "../public/home.png";
 import planet from "../public/planet.png";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function TopBar() {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLogged, setIsLogged] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const logged = localStorage.getItem("user");
+
+        if (logged !== null) {
+            setIsLogged(logged);
+            console.log(logged);
+        }
+    }, []);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (name === "Peke86" && password === "peke123") {
+            localStorage.setItem("user", "Peke86");
+            navigate(0);
+        } else if (name === "t" && password === "t") {
+            localStorage.setItem("user", "Tintti");
+            navigate(0);
+        } else {
+            alert(`Kirjautuminen epäonnistui!`);
+        }
+    };
+
+    const handleLogout = (event) => {
+        event.preventDefault();
+
+        localStorage.removeItem("user");
+        navigate(0);
+    };
+
     return (
         <div className="topBar">
-            <h1>Ulkoilmaharrastajat</h1>
-            <div className="nav">
-                <img src={home} alt="Home icon" />
-                <Link to={"/"}>Koti</Link>
-                <img src={planet} alt="planet icon" />
-                <Link to={"/hobbies"}>Harrastukset</Link>
-            </div>
+            <section>
+                <h1>Ulkoilmaharrastajat</h1>
+                <div className="nav">
+                    <img src={home} alt="Home icon" />
+                    <Link to={"/"}>Koti</Link>
+                    <img src={planet} alt="planet icon" />
+                    <Link to={"/hobbies"}>Harrastukset</Link>
+                </div>
+            </section>
+            <section>
+                {isLogged ? (
+                    <section className="user">
+                        <label>kirjautuneena: {isLogged}</label>
+
+                        <form onSubmit={handleLogout} className="logout">
+                            <input type="submit" value="kirjaudu ulos" />
+                        </form>
+                    </section>
+                ) : (
+                    <form onSubmit={handleSubmit}>
+                        <section className="row">
+                            <section className="field">
+                                <label>käyttäjänimi</label>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </section>
+                            <section className="field">
+                                <label>Salasana</label>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                />
+                            </section>
+                        </section>
+                        <input type="submit" value="kirjaudu" />
+                    </form>
+                )}
+            </section>
         </div>
     );
 }
