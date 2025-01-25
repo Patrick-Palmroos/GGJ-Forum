a4a44d297ffaa69fef0f3953216ede2004bdd6cd
Patrick-Palmroos <palmroos.patrick@gmail.com>
2025-01-25 16:15:49 +0200

    Change: section styling

--- forum/src/Home.jsx
+++ forum/src/Home.jsx
@@ -1,10 +1,11 @@
 import home from "./json/home.json";
 import { Link } from "react-router-dom";
 import TopBar from "./TopBar";
+import "./Sections.css";
 
 export default function Home() {
     return (
-        <div>
+        <div className="section">
             <TopBar />
             <h1>Home</h1>
             <ul>
