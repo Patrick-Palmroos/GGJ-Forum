27227ce587917554e9c4fb05e6e8ec454001e85c
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-26 03:11:50 +0200

    edit: login credentials

--- forum/src/Home.jsx
+++ forum/src/Home.jsx
@@ -3,44 +3,44 @@ import { Link } from "react-router-dom";
 import TopBar from "./TopBar";
 import "./Sections.css";
 import { useState, useEffect } from "react";
+import lock from "../public/lock.png";
 
 export default function Home() {
-    const [isLogged, setIsLogged] = useState(null);
+  const [isLogged, setIsLogged] = useState(null);
 
-    useEffect(() => {
-        const logged = localStorage.getItem("user");
+  useEffect(() => {
+    const logged = localStorage.getItem("user");
 
-        if (logged !== null) {
-            setIsLogged(logged);
-            console.log(logged);
-        }
-    }, []);
+    if (logged !== null) {
+      setIsLogged(logged);
+      console.log(logged);
+    }
+  }, []);
 
-    return (
-        <div className="section">
-            <TopBar />
-            <h1>Koti</h1>
-            <ul>
-                {home.sections.map((section, i) => (
-                    <li key={i}>
-                        {section.isOpen ? (
-                            <Link to={"/section"} state={{ section }}>
-                                {section.sectionName}
-                            </Link>
-                        ) : isLogged !== null &&
-                          section.users.includes(isLogged) ? (
-                            <Link to={"/section"} state={{ section }}>
-                                {section.sectionName}
-                            </Link>
-                        ) : (
-                            <div className="closed">
-                                <p>{section.sectionName}</p>
-                                <img src={lock} alt="lock symbol" />
-                            </div>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="section">
+      <TopBar />
+      <h1>Koti</h1>
+      <ul>
+        {home.sections.map((section, i) => (
+          <li key={i}>
+            {section.isOpen ? (
+              <Link to={"/section"} state={{ section }}>
+                {section.sectionName}
+              </Link>
+            ) : isLogged !== null && section.users.includes(isLogged) ? (
+              <Link to={"/section"} state={{ section }}>
+                {section.sectionName}
+              </Link>
+            ) : (
+              <div className="closed">
+                <p>{section.sectionName}</p>
+                <img src={lock} alt="lock symbol" />
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- forum/src/TopBar.jsx
+++ forum/src/TopBar.jsx
@@ -6,87 +6,85 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function TopBar() {
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
-    const [isLogged, setIsLogged] = useState(null);
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogged, setIsLogged] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const logged = localStorage.getItem("user");
+  useEffect(() => {
+    const logged = localStorage.getItem("user");
 
-        if (logged !== null) {
-            setIsLogged(logged);
-            console.log(logged);
-        }
-    }, []);
+    if (logged !== null) {
+      setIsLogged(logged);
+      console.log(logged);
+    }
+  }, []);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        if (name === "Peke86" && password === "peke123") {
-            localStorage.setItem("user", "Peke86");
-            navigate(0);
-        } else if (name === "t" && password === "t") {
-            localStorage.setItem("user", "Tintti");
-            navigate(0);
-        } else {
-            alert(`Kirjautuminen epäonnistui!`);
-        }
-    };
+    if (name === "Peke86" && password === "peke123") {
+      localStorage.setItem("user", "Peke86");
+      navigate(0);
+    } else if (name === "Suviheila" && password === "p35udHjj876vH") {
+      localStorage.setItem("user", "Suviheila");
+      navigate(0);
+    } else {
+      alert(`Kirjautuminen epäonnistui!`);
+    }
+  };
 
-    const handleLogout = (event) => {
-        event.preventDefault();
+  const handleLogout = (event) => {
+    event.preventDefault();
 
-        localStorage.removeItem("user");
-        navigate(0);
-    };
+    localStorage.removeItem("user");
+    navigate(0);
+  };
 
-    return (
-        <div className="topBar">
-            <section>
-                <h1>Ulkoilmaharrastajat</h1>
-                <div className="nav">
-                    <img src={home} alt="Home icon" />
-                    <Link to={"/"}>Koti</Link>
-                    <img src={planet} alt="planet icon" />
-                    <Link to={"/hobbies"}>Harrastukset</Link>
-                </div>
-            </section>
-            <section>
-                {isLogged ? (
-                    <section className="user">
-                        <label>kirjautuneena: {isLogged}</label>
-
-                        <form onSubmit={handleLogout} className="logout">
-                            <input type="submit" value="kirjaudu ulos" />
-                        </form>
-                    </section>
-                ) : (
-                    <form onSubmit={handleSubmit}>
-                        <section className="row">
-                            <section className="field">
-                                <label>käyttäjänimi</label>
-                                <input
-                                    type="text"
-                                    value={name}
-                                    onChange={(e) => setName(e.target.value)}
-                                />
-                            </section>
-                            <section className="field">
-                                <label>Salasana</label>
-                                <input
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) =>
-                                        setPassword(e.target.value)
-                                    }
-                                />
-                            </section>
-                        </section>
-                        <input type="submit" value="kirjaudu" />
-                    </form>
-                )}
-            </section>
+  return (
+    <div className="topBar">
+      <section>
+        <h1>Ulkoilmaharrastajat</h1>
+        <div className="nav">
+          <img src={home} alt="Home icon" />
+          <Link to={"/"}>Koti</Link>
+          <img src={planet} alt="planet icon" />
+          <Link to={"/hobbies"}>Harrastukset</Link>
         </div>
-    );
+      </section>
+      <section>
+        {isLogged ? (
+          <section className="user">
+            <label>kirjautuneena: {isLogged}</label>
+
+            <form onSubmit={handleLogout} className="logout">
+              <input type="submit" value="kirjaudu ulos" />
+            </form>
+          </section>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <section className="row">
+              <section className="field">
+                <label>käyttäjänimi</label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </section>
+              <section className="field">
+                <label>Salasana</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </section>
+            </section>
+            <input type="submit" value="kirjaudu" />
+          </form>
+        )}
+      </section>
+    </div>
+  );
 }
