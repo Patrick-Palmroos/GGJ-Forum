d35e19b659b6fdf5447ae3bc52d4a142668470d5
Sumu Vuori <sumu.vuori@tuni.fi>
2025-01-26 04:28:15 +0200

    fix password

--- forum/src/TopBar.jsx
+++ forum/src/TopBar.jsx
@@ -23,7 +23,7 @@ export default function TopBar() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    if (name === "Peke68" && password === "peke123") {
+    if (name === "Peke68" && password === "pekka123") {
       localStorage.setItem("user", "Peke68");
       navigate(0);
     } else if (name === "Suviheila" && password === "p35udHjj876vH") {
